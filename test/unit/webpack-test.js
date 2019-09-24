19da41eae59433c7f974451e9e752048ff34ae5a
chris-hly <qiang.fang@huilianyi.com>
2019-09-24 12:05:58 +0800

    fix(webpack-test): 修改测试用例

--- test/unit/webpack-test.js
+++ test/unit/webpack-test.js
@@ -4,7 +4,7 @@ describe('webpack test ', () => {
 
     
     it('entry', () => {
-        assert.equal(baseConfig.entry.expense, '/Users/fangqiang/Desktop/chris-learning/webpack/webpack-design/hly-webpack-builder/test/smoke/template/src/expense/index.js');
-        assert.equal(baseConfig.entry['my-account'], '/Users/fangqiang/Desktop/chris-learning/webpack/webpack-design/hly-webpack-builder/test/smoke/template/src/my-account/index.js');
+        assert.equal(baseConfig.entry.expense.indexOf('hly-webpack-builder/test/smoke/template/src/expense/index.js'> -1));
+        assert.equal(baseConfig.entry['my-account'].indexOf('hly-webpack-builder/test/smoke/template/src/my-account/index.js')> -1);
     });
 })

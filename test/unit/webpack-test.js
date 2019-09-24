dc82731dd8f76a7c76b433c553b5344ea6254fcc
chris-hly <qiang.fang@huilianyi.com>
2019-09-24 09:56:03 +0800

    fix:

--- test/unit/webpack-test.js
+++ test/unit/webpack-test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 describe('webpack test ', () => {
     const baseConfig = require('../../lib/webpack.base.js')
+
     
     it('entry', () => {
         assert.equal(baseConfig.entry.expense, '/Users/fangqiang/Desktop/chris-learning/webpack/webpack-design/hly-webpack-builder/test/smoke/template/src/expense/index.js');
